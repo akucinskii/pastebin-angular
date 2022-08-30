9366b5a3bb2c382cd6e02d6422c3372573437e54
artuyees <artuyees@gmail.com>
2022-08-31 00:01:07 +0200

    Updated console.log

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,13 +2,13 @@ import buildServer from "./server";
 
 const server = buildServer();
 
-const port = Number(process.env.PORT) || 3000;
+const port = Number(process.env.PORT) || 3001;
 
 async function main() {
   try {
     server.listen({ port: port, host: "0.0.0.0" });
 
-    console.log(`Server ready at http://localhost:3000`);
+    console.log(`Server ready at http://localhost:${port}`);
   } catch (e) {
     console.error(e);
     process.exit(1);
