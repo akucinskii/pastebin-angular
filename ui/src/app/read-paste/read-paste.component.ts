6ebe4978d1c96f6891aba6261083656bc578dea3
artuyees <artuyees@gmail.com>
2022-09-01 20:09:37 +0200

    Pastes are now displayed on read-paste page

--- ui/src/app/read-paste/read-paste.component.ts
+++ ui/src/app/read-paste/read-paste.component.ts
@@ -1,15 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+interface PasteInterface {
+  title: string;
+  content: string;
+}
 
 @Component({
   selector: 'app-read-paste',
   templateUrl: './read-paste.component.html',
-  styleUrls: ['./read-paste.component.css']
+  styleUrls: ['./read-paste.component.css'],
 })
 export class ReadPasteComponent implements OnInit {
+  public id: string | null;
+  public paste: PasteInterface;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
   ngOnInit(): void {
-  }
+    this.id = this.route.snapshot.paramMap.get('id');
 
+    this.http
+      .get(`http://localhost:3001/api/post/${this.id}`)
+      .subscribe((res: any) => {
+        this.paste = res;
+        console.log(res);
+      });
+  }
 }
