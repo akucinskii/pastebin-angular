83d608e2041a11aa5b4867ebb7acc40077a3f28f
artuyees <artuyees@gmail.com>
2022-09-03 22:54:10 +0200

    Added ngx-clipboard module

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ClipboardModule } from 'ngx-clipboard';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +11,7 @@ import { ReadPasteComponent } from './read-paste/read-paste.component';
 
 @NgModule({
   declarations: [AppComponent, CreatePasteComponent, ReadPasteComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, ClipboardModule],
   providers: [],
   bootstrap: [AppComponent],
 })
