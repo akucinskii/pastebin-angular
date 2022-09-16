8b21a0896bfebf8a19f93843593dd3680b5eb0e2
Artur Kucinski <artuyees@gmail.com>
2022-09-16 22:37:50 +0200

    Removed unused selector

--- ui/src/app/state/user.selectors.ts
+++ ui/src/app/state/user.selectors.ts
@@ -1,13 +1,6 @@
 import { User } from './user.model';
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 
-export const userLoginStatus = createSelector(
-  createFeatureSelector<User>('user'),
-  (state: User) => {
-    console.log(state);
-    return !state.isLoggedIn;
-  }
-);
 export const userSelector = createSelector(
   createFeatureSelector<User>('user'),
   (state: User) => {
